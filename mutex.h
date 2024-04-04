f1ab90204f217b2f5c0a8d110e4af81dd35081f2
Pythraithia <pythraithia@gmail.com>
2024-04-03 21:35:04 -0500

    new stuff

--- mutex.h
+++ mutex.h
@@ -1,3 +1,5 @@
+#ifndef __MUTEX_H__
+#define __MUTEX_H__
 #include "spinlock.h"
 
 // Some values are copied from sleeplock.h
@@ -5,3 +7,5 @@ typedef struct {
   uint locked;       // Is the lock held?
   struct spinlock lk; // spinlock protecting this sleep lock
 } mutex;
+
+#endif
